25fd8659973e9a0f9344fb5289a0f7ad41d74dd9
Brad <brad@viziworx.com>
2020-10-01 12:24:55 -0400

    feat: adds useEffect to get initial state from localStorage and keep localStorage updated as the name is updated

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -6,11 +6,16 @@ import React from 'react'
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
+  const [name, setName] = React.useState(
+    window.localStorage.getItem('name') || initialName,
+  )
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
+  React.useEffect(() => {
+    window.localStorage.setItem('name', name)
+  })
 
   function handleChange(event) {
     setName(event.target.value)
@@ -19,7 +24,7 @@ function Greeting({initialName = ''}) {
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
